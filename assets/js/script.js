2dde8a9735f13c17d1ee3f62b9252fe851080679
unknown <lopez1432.al@gmail.com>
2021-12-02 17:59:59 -0800

    complete header, footer, and body with recipe cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
 var formEL = document.querySelector("#search-option");
 
 formEL.addEventListener("submit", createTaskHandler);
+
+fetch("https://nutritionix-api.p.rapidapi.com/v1_1/item?upc=49000036756", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
+		"x-rapidapi-key": "91bafe7eabmsh13cab3f695cd767p111b8ejsn4a83ee5f5839"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+		"x-rapidapi-key": "91bafe7eabmsh13cab3f695cd767p111b8ejsn4a83ee5f5839"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+}); 
