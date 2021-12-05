5d219ca05320c12820f4435d6feeff2387b08176
sohara <steph.ohara@icloud.com>
2021-12-05 13:55:01 -0800

    added api code to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,57 @@
+// idirate through responses with forloop
+// create html with javascript
+// apend those elements to the html where I want them
+
 var formEL = document.querySelector("#search-option");
 
-formEL.addEventListener("submit", createTaskHandler);
+async function formSubmit(e) {
+  e.preventDefault();
+  var formInput = document.getElementById("search-input").value;
+  var formSelect = document.getElementById("search-type").value;
 
-fetch("https://nutritionix-api.p.rapidapi.com/v1_1/item?upc=49000036756", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
-		"x-rapidapi-key": "91bafe7eabmsh13cab3f695cd767p111b8ejsn4a83ee5f5839"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+  const recipeRes = await fetch(
+    "https://edamam-recipe-search.p.rapidapi.com/search?q=" + formInput,
+    {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+        "x-rapidapi-key": "fc2d74bc61msh2e94d023d3c4315p13df19jsn707909954210",
+      },
+    }
+  );
 
-fetch("https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
-		"x-rapidapi-key": "91bafe7eabmsh13cab3f695cd767p111b8ejsn4a83ee5f5839"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-}); 
+  if (!recipeRes.ok) {
+    console.error(recipeRes.status);
+  } else {
+    const recipeData = await recipeRes.json();
+    console.log(recipeData);
+  }
+
+  const response = await fetch(
+    "https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat",
+    {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
+        "x-rapidapi-key": "fc2d74bc61msh2e94d023d3c4315p13df19jsn707909954210",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    console.error(response.status);
+  } else {
+    const data = await response.json();
+    console.log(data);
+  }
+}
+
+formEL.onsubmit = formSubmit;
+
+// function formChange(e) {
+//   console.log(e.target.value)
+// }
+
+// formEL.onchange = formChange;
